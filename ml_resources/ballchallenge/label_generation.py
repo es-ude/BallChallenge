d29305dd0e71732e006d146423894df288b8acb9
Julian Hoever <julianhoever@posteo.de>
2024-01-18 12:03:50 +0100

    integrate real acceleromenter dataset
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- ml_resources/ballchallenge/label_generation.py
+++ ml_resources/ballchallenge/label_generation.py
@@ -1,10 +1,9 @@
 import numpy as np
+from math import sqrt
 
 
 def _gaussian_density(values: np.ndarray, mean: float, std: float) -> np.ndarray:
-    return (
-        1 / (std * np.sqrt(2 * np.pi)) * np.exp(-0.5 * (values - mean) ** 2 / std**2)
-    )
+    return 1 / (std * sqrt(2 * np.pi)) * np.exp(-0.5 * (values - mean) ** 2 / std**2)
 
 
 def _gaussian_density_2d(
@@ -19,27 +18,17 @@ def _gaussian_density_2d(
 def generate_smooth_labels(
     target_points: list[tuple[float, float]],
     target_std: float,
+    x_value_range: tuple[float, float],
+    y_value_range: tuple[float, float],
     grid_size: tuple[int, int],
 ) -> np.ndarray:
     labels = np.zeros((len(target_points), *grid_size), dtype=np.float32)
 
-    x_range = np.arange(grid_size[0])
-    y_range = np.arange(grid_size[1])
+    x_range = np.linspace(*x_value_range, grid_size[0])
+    y_range = np.linspace(*y_value_range, grid_size[1])
     x, y = np.meshgrid(x_range, y_range)
 
     for i, point in enumerate(target_points):
         labels[i] = _gaussian_density_2d(x, y, mean_point=point, std=target_std)
-
-    return labels
-
-
-def generate_hard_labels(
-    target_points: list[tuple[float, float]],
-    grid_size: tuple[int, int],
-) -> np.ndarray:
-    labels = np.zeros((len(target_points), *grid_size), dtype=np.float32)
-
-    for i, point in enumerate(target_points):
-        labels[i, *point[::-1]] = 1
 
     return labels
