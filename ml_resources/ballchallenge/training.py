ff2b3fd3724306adeaa20de96232fda2c374b05b
Julian Hoever <julianhoever@posteo.de>
2024-01-24 12:03:16 +0100

    feat(ml): remove accuracy metric to simplify code
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- ml_resources/ballchallenge/training.py
+++ ml_resources/ballchallenge/training.py
@@ -1,20 +1,9 @@
 from typing import Any
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 
 from .train_history import TrainHistory
-
-
-def _correct_predicted(
-    predicted_labels: torch.Tensor, target_labels: torch.Tensor
-) -> int:
-    def extract_class_indices(labels: torch.Tensor) -> torch.Tensor:
-        return torch.tensor([label.argmax() for label in labels])
-
-    predicted_indices = extract_class_indices(predicted_labels)
-    target_indices = extract_class_indices(target_labels)
-
-    return int((predicted_indices == target_indices).sum().item())
 
 
 def run_training(
@@ -39,8 +28,6 @@ def run_training(
         model.train()
 
         running_loss = 0
-        correct_predicted = 0
-        num_samples = 0
 
         for samples, labels in dl_train:
             samples = samples.to(device)
@@ -55,17 +42,12 @@ def run_training(
             optimizer.step()
 
             running_loss += loss.item()
-            correct_predicted += _correct_predicted(predictions, labels)
-            num_samples += len(samples)
 
         train_loss = running_loss / len(dl_train)
-        train_accuracy = correct_predicted / num_samples
 
         model.eval()
 
         running_loss = 0
-        correct_predicted = 0
-        num_samples = 0
 
         with torch.no_grad():
             for samples, labels in dl_test:
@@ -76,20 +58,14 @@ def run_training(
                 loss = loss_fn(predictions, labels)
 
                 running_loss += loss.item()
-                correct_predicted += _correct_predicted(predictions, labels)
-                num_samples += len(samples)
 
         test_loss = running_loss / len(dl_test)
-        test_accuracy = correct_predicted / num_samples
 
         history.log("epoch", epoch, epoch)
         history.log("loss", train_loss, test_loss)
-        history.log("accuracy", train_accuracy, test_accuracy)
 
         print(
-            f"[epoch {epoch}/{epochs}] "
-            f"train_loss: {train_loss:.04f} ; train_accuracy: {train_accuracy:.04f} ; "
-            f"test_loss: {test_loss:.04f} ; test_accuracy: {test_accuracy:.04f}"
+            f"[epoch {epoch}/{epochs}] train_loss: {train_loss:.04f} ; test_loss: {test_loss:.04f}"
         )
 
     return history
