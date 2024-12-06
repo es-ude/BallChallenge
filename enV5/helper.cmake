04ee11d2c08db39d0a38ff571a69418623cee1ea
David P. Federl <david-peter.federl@federl.digital>
2024-12-06 11:57:32 +0100

    fix(data-collect): update cmake depdency download
    CMake made `FetchConten_Declare` deprecated!
    Had to be replaced with `FetchContent_MakeAvailable`
    
    BREAKING CHANGE: CMake `FetchContent_MakeAvailable`

--- enV5/helper.cmake
+++ enV5/helper.cmake
@@ -7,7 +7,7 @@ function(load_enV5)
             GIT_REPOSITORY https://github.com/es-ude/elastic-ai.runtime.enV5.git
             GIT_TAG v4.2.1
     )
-    FetchContent_Populate(runtime_env5)
+    FetchContent_MakeAvailable(runtime_env5)
 
     message(NOTICE "add runtime.enV5 init scripts")
     include(${runtime_env5_SOURCE_DIR}/enV5_init.cmake)
